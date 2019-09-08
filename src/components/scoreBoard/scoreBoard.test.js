910ca381574adce98b875d9bd596426e7ad186e1
2019-Dev-190 <myprofile1307@gmail.com>
2019-09-08 11:01:56 +0530

    Handle love for both players

--- src/components/scoreBoard/scoreBoard.test.js
+++ src/components/scoreBoard/scoreBoard.test.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ScoreBoard from './ScoreBoard';
+import renderer from 'react-test-renderer';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+
+configure({ adapter: new Adapter() });
+
 
 describe('ScoreBoard', () => {
 
@@ -8,5 +15,20 @@ describe('ScoreBoard', () => {
         const div = document.createElement('div');
         ReactDOM.render(<ScoreBoard />, div);
         ReactDOM.unmountComponentAtNode(div);
+    });
+    it('renders first player score correctly', () => {
+        const firstPlayer = 0;
+        const rendered = renderer.create(
+            <ScoreBoard firstPlayer={firstPlayer} />
+        );
+        expect(rendered.toJSON()).toMatchSnapshot();
+    });
+
+    it('renders second player score correctly', () => {
+        const secondPlayer = 0;
+        const rendered = renderer.create(
+            <ScoreBoard secondPlayer={secondPlayer} />
+        );
+        expect(rendered.toJSON()).toMatchSnapshot();
     });
 });
